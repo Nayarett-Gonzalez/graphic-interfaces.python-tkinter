ee25ffdb809ef4099ce57a7924a74b1a8e3f9ae5
NayarettGonzalez <nagonzalezv@uc.cl>
2021-03-07 14:47:58 -0300

    122-Cargando favicon desde cualquier lugar utilizando os.pathabspath()

--- 01-ventanas.py
+++ 01-ventanas.py
@@ -2,14 +2,25 @@
 Módulo para crear interfaces gráficas de usuario
 """
 from tkinter import *
+import os.path
+
 # Crear ventana raíz (principal)
 ventana = Tk()
 
 # Colocando título de la ventana
 ventana.title("Interfaz gráfica")
 
+# Comprobar si existe archivo de ícono
+ruta_icono = os.path.abspath(("./images/favicon.ico"))
+if not os.path.isfile(ruta_icono):
+    ruta_icono = os.path.abspath(("./images/favicon.ico"))
+
+# Mostrar texto en el programa
+texto = Label(ventana,text=ruta_icono)
+texto.pack()
+
 # Colocando un favicon a la ventana
-ventana.iconbitmap("./images/favicon.ico")
+ventana.iconbitmap(ruta_icono)
 
 # Establezco el tamaño inicial de la ventana
 ventana.geometry("750x450")
