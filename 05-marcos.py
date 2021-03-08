87dde5607bdad07b67265051778924fbf3d18cbf
NayarettGonzalez <nagonzalezv@uc.cl>
2021-03-08 19:54:36 -0300

    131-Texto dentro de frames en distintos lugares

--- 05-marcos.py
+++ 05-marcos.py
@@ -3,7 +3,7 @@ from tkinter import *
 ventana = Tk()
 
 ventana.title("Marcos | Master en Python")
-ventana.geometry("700x700")
+ventana.geometry("500x500")
 
 # Marco padre superior
 marco_padre = Frame(ventana, width=250,height=250)
@@ -15,24 +15,35 @@ marco_padre = Frame(ventana, width=250,height=250)
 marco_padre.pack(side=TOP,anchor=N,fill=X, expand=YES) # Necesario para que se vea el marco
 
 # Marco 1 superior
-marco = Frame(marco_padre, width=250,height=250)
+marco = Frame(marco_padre, width=150,height=150)
 marco.config(
     bg="red",bd=2,
     # relief=SOLID
     relief="raised"
 )
 marco.pack(side=LEFT) # Necesario para que se vea el marco
+# Para que no se alteren las dimensiones del marco con el texto que se le incluirá
+marco.pack_propagate(False)
+
+# Para incluir texto
+Label(marco,text="Primer Marco").pack()
+
 
 # Marco 2 superior
-marco = Frame(marco_padre, width=250,height=250)
+marco = Frame(marco_padre, width=150,height=150)
 marco.config(
     bg="green",bd=2,
     relief=SOLID
 )
 marco.pack(side=RIGHT) # Necesario para que se vea el marco
+# Para que no se alteren las dimensiones del marco con el texto que se le incluirá
+marco.pack_propagate(False)
+# Para incluir texto
+Label(marco,text="Segundo Marco").pack(side=TOP,anchor=NE)
 
+######################################################
 # Marco Padre Inferior
-marco_padre = Frame(ventana, width=250,height=250)
+marco_padre = Frame(ventana, width=150,height=150)
 # marco_padre.config(
 #     bg="#199845"
 #     # bd=5,
@@ -41,20 +52,26 @@ marco_padre = Frame(ventana, width=250,height=250)
 marco_padre.pack(side=BOTTOM,anchor=S,fill=X, expand=YES) # Necesario para que se vea el marco
 
 # Marco 3 
-marco = Frame(marco_padre, width=250,height=250)
+marco = Frame(marco_padre, width=150,height=150)
 marco.config(
     bg="blue",bd=2,
     relief=SOLID
 )
-marco.pack(side=RIGHT) # Necesario para que se vea el marco
+marco.pack(side=LEFT) # Necesario para que se vea el marco
+# Para que no se alteren las dimensiones del marco con el texto que se le incluirá
+marco.pack_propagate(False)
+# Para incluir texto
+Label(marco,text="Tercer Marco").pack(side=BOTTOM,anchor=SW)
 
 # Marco 4 
-marco = Frame(marco_padre, width=250,height=250)
+marco = Frame(marco_padre, width=150,height=150)
 marco.config(
     bg="pink",bd=2,
     relief=SOLID
 )
-marco.pack(side=LEFT) # Necesario para que se vea el marco
-
-
+marco.pack(side=RIGHT) # Necesario para que se vea el marco
+# Para que no se alteren las dimensiones del marco con el texto que se le incluirá
+marco.pack_propagate(False)
+# Para incluir texto
+Label(marco,text="Cuarto Marco").pack(side=BOTTOM,anchor=SE)
 ventana.mainloop()
