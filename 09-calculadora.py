46eebb56a0212eba91e760d85f04f2f55bac3799
NayarettGonzalez <nagonzalezv@uc.cl>
2021-03-10 00:03:23 -0300

    144- Calculadora; Operaciones vinculadas al formulario

--- 09-calculadora.py
+++ 09-calculadora.py
@@ -18,6 +18,31 @@ ventana.config(
 numero1 = StringVar()
 numero2 = StringVar()
 resultado = StringVar()
+def sumar():
+    resultado.set(float(numero1.get())+float(numero2.get()))
+    mostrarResultado()
+
+def restar():
+    resultado.set(float(numero1.get())-float(numero2.get()))
+    mostrarResultado()
+
+def multiplicar():
+    resultado.set(float(numero1.get())*float(numero2.get()))
+    mostrarResultado()
+
+def dividir():
+    resultado.set(float(numero1.get())/float(numero2.get()))
+    mostrarResultado()
+
+def mostrarResultado():
+    MessageBox.showinfo("Resultado", f"El resultado de la operación es {resultado.get()}")
+    # Borramos los números ingresados despues de realizar alguna de las operaciones
+    numero1.set("")
+    numero2.set("")
+
+
+
+
 
 
 marco = Frame(ventana,width=250,height=250)
@@ -37,10 +62,10 @@ Label(marco,text="Segundo Número: ").pack()
 Entry(marco, textvariable=numero2,justify="center").pack()
 Label(marco, text="")
 
-Button(marco,text="Sumar").pack(side="left",fill=X, expand=YES)
-Button(marco,text="Restar").pack(side="left",fill=X, expand=YES)
-Button(marco,text="Multiplicar").pack(side="left",fill=X, expand=YES)
-Button(marco,text="Dividir").pack(side="left",fill=X, expand=YES)
+Button(marco,text="Sumar",command=sumar).pack(side="left",fill=X, expand=YES)
+Button(marco,text="Restar",command=restar).pack(side="left",fill=X, expand=YES)
+Button(marco,text="Multiplicar",command=multiplicar).pack(side="left",fill=X, expand=YES)
+Button(marco,text="Dividir",command=dividir).pack(side="left",fill=X, expand=YES)
 
 
 
