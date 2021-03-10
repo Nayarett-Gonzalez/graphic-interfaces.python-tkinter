2797367f607943918a2c79d96113436d7ee90ab2
NayarettGonzalez <nagonzalezv@uc.cl>
2021-03-10 01:32:07 -0300

    148-checkbox

--- 11-formularios2.py
+++ 11-formularios2.py
@@ -12,10 +12,38 @@ encabezado.config(
     font=("Consolas",20)
 )
 
-encabezado.pack(side=TOP,anchor=N,fill=X,expand=YES)
+# encabezado.pack(side=TOP,anchor=N,fill=X,expand=YES)
+encabezado.grid(row=0,column=0,columnspan=5,sticky=W)
 
-# Botones check
+def mostrarProfesion():
+    texto=""
+
+    if web.get():
+        texto += "Desarrollo web"
+
+    if movil.get():
+        if web.get():
+            texto += " y desarrollo móvil"
+        else:
+            texto += "Desarrollo móvil"
+
+    mostrar.config(text=texto,bg="green",fg="white")
+
+web=IntVar()
+movil=IntVar()
+# Botones check (checkbox en HTML)
+# Label(ventana,text="¿A qué te dedicas?").pack()
+Label(ventana,text="¿A qué te dedicas?").grid(row=1,column=0,sticky=W)
+Checkbutton(ventana, text="Desarrollo web",variable=web, onvalue=1,offvalue=0,command=mostrarProfesion).grid(row=2,column=0,sticky=W)
+Checkbutton(ventana, text="Desarrollo móvil",variable=movil, onvalue=1, offvalue=0,command=mostrarProfesion).grid(row=3,column=0,sticky=W)
+
+
+mostrar = Label(ventana)
+mostrar.grid(row=4,column=0)
+
 # Radio buttons
+
+
 # Option Menu
 
 ventana.mainloop()
